6190cb3c1be55376aa9d7e83353a5fb1ef113a7d
Camila Pinzon <camilapinz18@gmail.com>
2022-11-21 17:27:03 -0500

    Studies

--- src/Components/Studies/Studies.jsx
+++ src/Components/Studies/Studies.jsx
@@ -1,11 +1,35 @@
 import './studies.css'
 
-const Studies=()=>{
-    return(
+const Studies = () => {
+    return (
         <section id="studies">
-            
+
             <div className='container studies_container'>
-            <h1>Studies</h1>
+                <h1>Studies</h1>
+                <div className='studies_flex'>
+                    <div className='studies_image'>
+                        <h1>Aqui va una immagen</h1>
+                    </div>
+                    <div className='studies_list'>
+                        <div className='studies_list_element'>
+                            <h3>Curso</h3>
+                            <h4>Detalles</h4>
+                        </div>
+                        <div className='studies_list_element'>
+                            <h3>Curso</h3>
+                            <h4>Detalles</h4>
+                        </div>
+                        <div className='studies_list_element'>
+                            <h3>Curso</h3>
+                            <h4>Detalles</h4>
+                        </div>
+                        <div className='studies_list_element'>
+                            <h3>Curso</h3>
+                            <h4>Detalles</h4>
+                        </div>
+                    </div>
+
+                </div>
             </div>
         </section>
     )
